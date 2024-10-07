46eb5808f344b6d622dde3848527d9c89b359061
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-10-07 12:48:40 +0900

    01_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int Recursive(int n) {
+	if (n <= 1) {
+		return(100);
+	}
+
+	return (Recursive(n - 1) + Recursive(n - 1) * 2 - 50);
+}
+
 int main(){
 
+	int a = 1072;
+	int n = 5;
+	int result;
+
+	result = a * n;
+	printf("ˆê”Ê“I‚È’À‹à‚Ì%dŽžŠÔ•ª = %d\n", n, result);
+	result = Recursive(n);
+	printf("Ä‹A“I‚È’À‹à‚Ì%dŽžŠÔ•ª = %d\n", n, result);
 	return 0;
 }
