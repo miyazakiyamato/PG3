084037cd12161a54e7890847b044e73e1a1226b3
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-10-21 16:39:06 +0900

    06_02_資料の内容確認

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <thread>
+
+void Double(int num) {
+	num *= 2;
+	printf("num * 2; %d\n", num);
+}
+
+void AddTwo(int num) {
+	num += 2;
+	printf("num + 2; %d\n", num);
+}
+
+void Quadruple(int num) {
+	num *= 4;
+}
 
 int main(){
+	int num = 3;
+	//マルチスレッド
+	std::thread th1(Double, num);
+	std::thread th2(AddTwo, num);
+
+	th1.join();
+	th2.join();
 
 	return 0;
 }
