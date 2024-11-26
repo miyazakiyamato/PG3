508f8e13cb6af9b8a77f0ab43f8f4297fae6a1fc
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-11-26 16:00:45 +0900

    03_01_STLとコンテナクラス

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include "list"
+#include "array"
+
+void SetList(std::list<const char*>& yamanoteLine,const char* setStation,const char* nextStation) {
+	for (std::list<const char*>::iterator it = yamanoteLine.begin(); it != yamanoteLine.end(); it++) {
+		if (*it == nextStation) {
+			it = yamanoteLine.insert(it, setStation);
+			it++;
+		}
+	}
+}
+
+void Draw(std::list<const char*> yamanoteLine) {
+	int index = 1;
+	for (const char* station : yamanoteLine) {
+		printf("JY%2d:", index);
+		printf("%s\n", station);
+		index++;
+	}
+	printf("\n");
+}
 
 int main(){
+
+	//º˜a3”N‚Ì˜Hü}
+	std::list<const char*> yamanoteLine{
+		 "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno","Uguisudani", "Nippori", "Tabata", "Komagome",
+		 "Sugamo","Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo","Shinjuku", "Yoyogi", "Harajuku","Shibuya",
+		 "Ebisu","Meguro", "Gotanda", "Osaki", "Shinagawa", "Tamachi","Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
+	printf("1970”N‚Ì˜Hü}\n");
+	Draw(yamanoteLine);
+	//º˜a47”N‚Ì˜Hü}
+	SetList(yamanoteLine, "Nishi-Nippori", "Tabata");
+	printf("2019”N‚Ì˜Hü}\n");
+	Draw(yamanoteLine);
+	//º˜a47”N‚Ì˜Hü}
+	SetList(yamanoteLine, "Takanawa Gateway", "Tamachi");
+	printf("2022”N‚Ì˜Hü}\n");
+	Draw(yamanoteLine);
 
 	return 0;
 }
