29f49f2cff0a628542d50b666e35bee4b61729fd
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-12-20 16:44:22 +0900

    06_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
-void Double(int num) {
-	num *= 2;
-	printf("num * 2; %d\n", num);
-}
+std::mutex mtx;
+std::condition_variable cv;
+int current = 1;
 
-void AddTwo(int num) {
-	num += 2;
-	printf("num + 2; %d\n", num);
-}
-
-void Quadruple(int num) {
-	num *= 4;
+void ThreadNum(int num) {
+	std::unique_lock<std::mutex> lock(mtx);
+	cv.wait(lock, [&]() { return current == num; }); // num が現在のターンであることを確認
+	printf("thread%d\n", num);
+	current++; // 次のターンに進む
+	cv.notify_all(); // 他のスレッドに通知
 }
 
 int main(){
-	int num = 3;
 	//マルチスレッド
-	std::thread th1(Double, num);
-	std::thread th2(AddTwo, num);
+	std::thread th1(ThreadNum, 1);
+	std::thread th2(ThreadNum, 2);
+	std::thread th3(ThreadNum,3);
 
 	th1.join();
 	th2.join();
+	th3.join();
 
 	return 0;
 }
